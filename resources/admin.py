17ff485e9bb32eb0c6a9784edb2318457d4a11d4
ckoerber <software@ckoerber.com>
2020-04-20 19:41:45 +0200

    Added admin pages

--- resources/admin.py
+++ resources/admin.py
@@ -1,3 +1,53 @@
-from django.contrib import admin
+"""Django admin setup for models
+"""
 
-# Register your models here.
+from django.contrib.admin import register, ModelAdmin
+
+from resources.models import (
+    Supplier,
+    SupplierList,
+    SupplierReview,
+    Product,
+    ProductDelivery,
+    RemoteDatabase,
+)
+
+
+@register(Product)
+class ProductAdmin(ModelAdmin):
+    list_display = ("name",)
+
+
+@register(Supplier)
+class SupplierAdmin(ModelAdmin):
+    list_display = ("name", "addresses", "company_type", "trustworty")
+
+
+@register(SupplierReview)
+class SupplierReviewAdmin(ModelAdmin):
+    list_display = ("supplier", "source", "user", "date_added", "trustworty")
+
+
+@register(SupplierList)
+class SupplierListAdmin(ModelAdmin):
+    list_display = ("name",)
+
+
+@register(ProductDelivery)
+class ProductDelivery(ModelAdmin):
+    list_display = (
+        "product",
+        "receiver",
+        "amount",
+        "date_send",
+        "date_received",
+    )
+
+
+@register(RemoteDatabase)
+class RemoteDataBaseAdmin(ModelAdmin):
+    list_display = (
+        "name",
+        "address",
+        "supplements",
+    )
