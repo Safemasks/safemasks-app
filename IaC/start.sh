2f1fc93ea21afa5bace69655aaece365968afb87
smkhassan <futureco@email.cz>
2020-07-14 12:02:21 +0200

    letsencrypt

--- IaC/start.sh
+++ IaC/start.sh
@@ -11,7 +11,11 @@ else
     echo "There is no script $PRE_START_PATH"
 fi
 
+git clone https://github.com/letsencrypt/letsencrypt ./letsencrypt
+
+#run the installation , nginx config for SSL has been set up in entrypoint.sh
+./letsencrypt/letsencrypt-auto certonly --standalone --email samimkhassan@gmail.com --agree-tos --no-eff-email -d portal.safemasks.de
 
 
-# Start Supervisor, with Nginx and uWSGI
+# Start Supervisor, Django
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
