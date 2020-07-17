6799dc86a5ee750f91e7ff9312230bd4353f9e9f
Christopher Koerber <software@ckoerber.com>
2020-07-17 14:36:09 +0200

    Adjusted url names

--- safemasks/resources/urls.py
+++ safemasks/resources/urls.py
@@ -14,10 +14,10 @@ from safemasks.resources.views import (
 app_name = "resources"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
-    path("products/list/", ProductListView.as_view(), name="product-list"),
+    path("products/", ProductListView.as_view(), name="product-list"),
     path("products/detail/", ProductDetailView.as_view(), name="product-detail"),
     path("products/trusted/", TrustedProductView.as_view(), name="product-trusted"),
-    path("suppliers/list/", SupplierListView.as_view(), name="supplier-list"),
+    path("suppliers/", SupplierListView.as_view(), name="supplier-list"),
     path("suppliers/detail/", SupplierDetailView.as_view(), name="supplier-detail"),
     path("api/", include((ROUTER.urls, "resources"), namespace="api")),
 ]
