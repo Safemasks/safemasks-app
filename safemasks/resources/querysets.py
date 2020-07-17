e4575f726f959c880a53a0a34e599383c5f62a2c
Christopher Koerber <software@ckoerber.com>
2020-07-17 18:02:22 +0200

    Added query for trusted suppliers

--- safemasks/resources/querysets.py
+++ safemasks/resources/querysets.py
@@ -1,6 +1,8 @@
 """Collects querries reused over app."""
-from django.db.models import Count, Avg, Max
+from django.db.models import Count, Avg, Max, Q
 from django.db.models.functions import Coalesce
+
+from safemasks.resources.models import Supplier
 
 
 def annotate_suppliers(qs):
@@ -23,4 +25,27 @@ def annotate_reviews(qs):
         qs.annotate(n_reviews_qs=Count("reviews"))
         .annotate(latest_review_qs=Max("reviews__last_update"))
         .annotate(avg_rating_qs=Avg("reviews__rating"))
+    )
+
+
+def trustworty_suppliers():
+    """Returns all trusted suppliers.
+
+    A supplier is trusted if either the average product or average supplier rating
+    is above 0.5. If one rating is above 0.5 and the other is below 0.5, the supplier
+    is not trusted.
+    """
+    positive_rating = Q(avg_rating_qs__isnull=False) & Q(avg_rating_qs__gte=0.5)
+    positive_product_rating = Q(avg_product_rating_qs__isnull=False) & Q(
+        avg_product_rating_qs__gte=0.5
+    )
+    negative_rating = Q(avg_rating_qs__isnull=False) & Q(avg_rating_qs__lt=-0.5)
+    negative_product_rating = Q(avg_product_rating_qs__isnull=False) & Q(
+        avg_product_rating_qs__lt=-0.5
+    )
+    return (
+        annotate_suppliers(Supplier.objects.all())
+        .filter(positive_rating | positive_product_rating)
+        .exclude(negative_rating)
+        .exclude(negative_product_rating)
     )
